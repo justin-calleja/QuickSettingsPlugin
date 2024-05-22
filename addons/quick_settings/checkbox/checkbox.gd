e72994a9b5b6c0ee6600b8505c64b479ce4d96e5
Justin Calleja <calleja.justin@gmail.com>
2024-05-22 08:27:16 +0200

    fix errors in log output and react to project settings change

--- addons/quick_settings/checkbox/checkbox.gd
+++ addons/quick_settings/checkbox/checkbox.gd
@@ -8,7 +8,7 @@ extends Control
 @export var _linkBtnText: String:
 	set(new_linkBtnText):
 		_linkBtnText = new_linkBtnText
-		link_button.text = _linkBtnText
+		if link_button: link_button.text = _linkBtnText
 @export var _setting_path: String
 @export var _setting_type: SettingType = SettingType.PROJECT
 
@@ -17,7 +17,7 @@ var editor_settings = EditorInterface.get_editor_settings()
 
 
 func get_setting() -> Variant:
-	if _setting_type == SettingType.EDITOR:
+	if _setting_type == SettingType.EDITOR and editor_settings:
 		return editor_settings.get(_setting_path)
 	else:
 		return ProjectSettings.get_setting(_setting_path)
@@ -34,6 +34,7 @@ func _ready():
 	link_button.text = _linkBtnText
 	check_box.set_pressed_no_signal(get_setting())
 	editor_settings.settings_changed.connect(_on_settings_changed)
+	ProjectSettings.settings_changed.connect(_on_settings_changed)
 
 
 func _on_link_button_pressed() -> void:
@@ -42,8 +43,9 @@ func _on_link_button_pressed() -> void:
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	set_setting(toggled_on)
-	#print('Plugin<quick_settings>: $s %s' % ["enabled" if toggled_on else "disabled", _setting_path])
+	#print('Plugin<quick_settings>: %s %s' % ["enabled" if toggled_on else "disabled", _setting_path])
 
 func _on_settings_changed() -> void:
-	# print("in _on_settings_changed with get of: %s" % get_setting()
-	check_box.set_pressed_no_signal(get_setting())
+	var _setting = get_setting()
+	check_box.set_pressed_no_signal(false if _setting == null else _setting)
+	#print("in _on_settings_changed with get of: %s" % _setting)
