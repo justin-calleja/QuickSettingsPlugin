44d480b8e7391088dfd09be91d1c082a91546d9a
Justin Calleja <calleja.justin@gmail.com>
2024-06-07 20:52:28 +0200

    fix set_dropdown_with_checkbox.gd script

--- addons/quick_settings/checkbox/set_dropdown_with_checkbox.gd
+++ addons/quick_settings/checkbox/set_dropdown_with_checkbox.gd
@@ -8,5 +8,10 @@ enum DropdownOption { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH }
 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
-		if toggled_on:
-				set_setting(_toggled_dropdown_opt)
+	if toggled_on:
+		set_setting(_toggled_dropdown_opt)
+
+
+func _on_settings_changed() -> void:
+	var _setting = get_setting()
+	check_box.set_pressed_no_signal(_setting == _toggled_dropdown_opt)
