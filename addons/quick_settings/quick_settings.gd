118821ac82e1917d9181969712d4c1dc40e415d7
Justin Calleja <calleja.justin@gmail.com>
2024-06-08 07:11:13 +0200

    make dock private

--- addons/quick_settings/quick_settings.gd
+++ addons/quick_settings/quick_settings.gd
@@ -1,21 +1,21 @@
 @tool
 extends EditorPlugin
 
-var dock
+var _dock
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Load the dock scene and instantiate it.
-	dock = preload("dock.tscn").instantiate()
+	_dock = preload("dock.tscn").instantiate()
 
 	# Add the loaded scene to the docks.
-	add_control_to_dock(DOCK_SLOT_LEFT_UR, dock)
+	add_control_to_dock(DOCK_SLOT_LEFT_UR, _dock)
 	# Note that LEFT_UL means the left of the editor, upper-left dock.
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	# Remove the dock.
-	remove_control_from_docks(dock)
+	remove_control_from_docks(_dock)
 	# Erase the control from the memory.
-	dock.free()
+	_dock.free()
